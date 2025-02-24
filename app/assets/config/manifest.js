d51851bb33754812ff24c7f1ac0925c379e1fa77
kamaliabadrina <badrinakamalia@gmail.com>
2025-02-24 11:59:32 +0800

    Error stylesheet

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link application.scss
 //= link application.js
-
+//= link application.scss
 //= link application.css
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
