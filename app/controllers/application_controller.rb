7780ad94362fbdc0d898014e4a12e18dd825d856
kamaliabadrina <badrinakamalia@gmail.com>
2025-02-24 12:06:00 +0800

    Error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :buy, :new, :create]
 end
