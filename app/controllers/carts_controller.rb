bb38608b20c178fa55f79abd6f52ed8d89a2ee11
kamaliabadrina <badrinakamalia@gmail.com>
2025-02-25 10:25:31 +0800

    Add to cart with option selected

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,39 +9,43 @@ class CartsController < ApplicationController
       @cart_items = @cart.cart_items
     else
       session[:cart] ||= {}
-      @cart_items = session[:cart].map do |product_id, quantity|
+      @cart_items = session[:cart].map do |product_id, data|
         product = Product.find_by(id: product_id)
-        OpenStruct.new(product: product, quantity: quantity, id: product_id) if product
+        OpenStruct.new(product: product, quantity: data["quantity"], id: product_id, options: data["options"]) if product
       end.compact
     end
   end
 
   def add_item
     product = Product.find_by(id: params[:product_id])
-
+  
     if product.nil?
       redirect_to cart_path, alert: "Product not found!" and return
     end
-
+  
     quantity = params[:quantity].to_i
     if quantity < 1
       redirect_to cart_path, alert: "Invalid quantity!" and return
     end
-
+  
+    selected_options = params[:options] || {} # Assuming options are passed as params
+  
     if user_signed_in?
       @cart = current_user.cart || current_user.create_cart
       @cart_item = @cart.cart_items.find_or_initialize_by(product_id: product.id)
       @cart_item.quantity += quantity
+      @cart_item.options = selected_options.to_json # Store selected options as JSON
       @cart_item.save
     else
       session[:cart] ||= {}
-      session[:cart][product.id.to_s] ||= 0
-      session[:cart][product.id.to_s] += quantity
+      session[:cart][product.id.to_s] ||= { "quantity" => 0, "options" => {} }
+      session[:cart][product.id.to_s]["quantity"] += quantity
+      session[:cart][product.id.to_s]["options"] = selected_options.to_json # Ensure options are stored as JSON
     end
-
+  
     redirect_to cart_path, notice: "Product added to cart!"
   end
-
+  
   def remove_item
     if user_signed_in?
       cart_item = CartItem.find_by(id: params[:id])
