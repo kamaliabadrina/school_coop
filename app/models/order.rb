64a4473ce3d0d2448f6345c28ddd402919016ae1
kamaliabadrina <badrinakamalia@gmail.com>
2025-03-05 12:23:29 +0800

    Change local to domain name for redirect payment link

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
 
   def generate_checksum
     Rails.logger.info("Generating checksum for order #{id}")
-    string = "#{email}|#{kid_name}|#{phone_number}||#{id}|#{product.name}|http://localhost:3000/orders/#{id}/paymentredirect|#{price}|b66c2a38-52f5-4e34-b1a3-520cf5741191"
+    string = "#{email}|#{kid_name}|#{phone_number}||#{id}|#{product.name}|http://kamalia.p.my/orders/#{id}/paymentredirect|#{price}|b66c2a38-52f5-4e34-b1a3-520cf5741191"
     puts(string)
     checksum_token = "4c30de911cb3a5f1957e9ee8f5acd285f5a464eb7e6fec89d301537393565b53"
     OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), checksum_token, string)
